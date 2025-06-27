1beb8d622f5bb6d70362fd66222c43563d46fca0
ShreyaGhemad <shreyaghemad16@gmail.com>
2025-06-27 19:27:15 +0530

    Add files via upload
    use terrnary operator in main

--- Linersearch.java
+++ Linersearch.java
@@ -13,11 +13,6 @@ public class Linersearch {
     public static void main(String[] args) {
         Integer[] arr = {11, 22, 33, 55, 44, 88, 66};
         Integer result = isLinearSearch(arr, 88);
-
-        if (result != null) {
-            System.out.println("Element found: " + result);
-        } else {
-            System.out.println("Element not found");
-        }
+        System.out.println(result != null ?" element is found ":" element is not found");
     }
 }
